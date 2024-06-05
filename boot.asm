37e618009c5d15b41b1b7c6fdeb82487d0c574f8
ajeetkumar95461857 <ajeetkumar95461857@gmail.com>
2024-06-05 18:32:11 +0530

    added comment to Message & MessageLen

--- boot.asm
+++ boot.asm
@@ -25,7 +25,8 @@ End:
     hlt // hlt instruction placess the processor in halt state
     jmp End  // if interrupt will resume execution so if interrupt is fired the processor will run the instruction following hlt which is jump to End means infinite loop
 
-Message:    db "Ajeet"
-MessageLen: equ $-Message
+Message:    db "Ajeet"  // db(define bytes)
+MessageLen: equ $-Message // equ is used to assign a name to value
+
 
 
