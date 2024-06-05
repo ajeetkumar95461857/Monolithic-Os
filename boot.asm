7809dc0a902817728f6ea38cfbdb7ed7801dc1cd
ajeetkumar95461857 <ajeetkumar95461857@gmail.com>
2024-06-05 17:52:33 +0530

    added start label code

--- boot.asm
+++ boot.asm
@@ -2,3 +2,10 @@
 
 [ORG 0x7c00]    // BIOS find the boot device and read first sector which is boot sector at 
                 // memory location 7c00 and control is transfer to it 
+
+start:
+    xor ax,ax   // xor makes value 0 with itself
+    mov ds,ax   // move 0 to data segement register which is pointer to data
+    mov es,ax   // move 0 to extra data segment register which is pointer to extra data
+    mov ss,ax   // mov 0 to stack segment which is pointer to the stack
+    mov sp,0x7c00
